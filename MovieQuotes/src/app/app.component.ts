0be70b1b8d601bc1355f6ff6772b28c5193225a0
matsubrasil <matsuboy@outlook.com>
2017-06-25 23:35:13 -0300

    data binding to a MovieQuote

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface MovieQuote {
+  movie: string;
+  quote: string;
+}
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movie = 'The Lego Movie';
+  movieQuote: MovieQuote = {
+    movie: '',
+    quote: ''
+  };
 }
