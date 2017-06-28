9a94e475ba1577768fc8ee2d801e1fc9e9e72bfd
matsubrasil <matsuboy@outlook.com>
2017-06-27 21:24:00 -0300

    finishing the local app

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -13,14 +13,23 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movieQuote: MovieQuote = {
+  formMovieQuote: MovieQuote = {
     movie: '',
     quote: ''
   };
 
+  movieQuotes: Array<MovieQuote> = [
+    { "movie": "Rocky", "quote": "Yo Adrian" },
+    { "movie": "Terminator", "quote": "I'll be back" },
+    { "movie": "Titanic", "quote": "I'm the king of the world!" },
+    { "movie": "The Princess Bride", "quote": "Hello, My name is Inigo Montoya. You killed my father. Prepare to die" },
+    
+  ];
+
   onSubmit(): void {
-    console.log('you wish to submit:', this.movieQuote );
-    this.movieQuote = {
+    // console.log('you wish to submit:', this.formMovieQuote );
+    this.movieQuotes.unshift(this.formMovieQuote);
+    this.formMovieQuote = {
       movie: '',
       quote: ''
     };
